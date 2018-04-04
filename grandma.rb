761c64981a7c8ca4f73787afd2b12f720885bac7
Alejandro Sabogal <asabogal.git@gmail.com>
2018-04-04 21:51:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,11 +2,15 @@ def speak_to_grandma(phrase)
 
 if phrase == "I LOVE YOU GRANDMA!"
   "I LOVE YOU TOO PUMPKIN!"
-end
 
-phrase == phrase.downcase ? "HUH?! SPEAK UP, SONNY!" : "NO, NOT SINCE 1938!"
-end
+elsif phrase == phrase.downcase
+  "HUH?! SPEAK UP, SONNY!"
 
+else
+  "NO, NOT SINCE 1938!"
+  end
+
+end
 
 
 
