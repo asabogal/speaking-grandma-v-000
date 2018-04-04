0c8e8313a7dc6503176e0864709a1a76d36860a2
Alejandro Sabogal <asabogal.git@gmail.com>
2018-04-04 21:31:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,11 +3,11 @@ def speak_to_grandma(phrase)
 if phrase == "I LOVE YOU GRANDMA!"
   "I LOVE YOU TOO PUMPKIN!"
 elsif
-  phrase.upcase
-
-  "NO, NOT SINCE 1938!"
-else
+  phrase.downcase
   "HUH?! SPEAK UP, SONNY!"
+  
+else
+  "NO, NOT SINCE 1938!"
 end
 end
 
